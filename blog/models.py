7cef39a639f1a545359bda9ae1f9ec4f799506c6
Lilit Movsisyan <lilit.momo@gmail.com>
2018-09-29 16:33:57 +0100

    Add template tags

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
     date_published = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.date_published = timezone.now()
         self.save()
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #we define a QuerySet, posts. This will be passed to the template rendered by tis view function.
+    posts = Post.objects.filter(date_published__lte=timezone.now()).order_by('date_published')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    #the render function takes 3 arguments:
+    #1. request = everything we receive from the user via the internet.
+    #2. the template file.
+    #3. {} = a place in which we can add things for the template to use. This is a dictionary. 
+    #so pass the posts variable defined above into this dictionary with a key: {'posts': posts}
