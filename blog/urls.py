f107330664e34e0d916726e7c1a865ea43e151e7
Lilit Movsisyan <lilit.momo@gmail.com>
2018-09-30 11:59:09 +0100

    Create post/new/ page

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new', views.post_new, name='post_new'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
 
 #the url path says '<int:pk>/' meaning a variable called pk will be passed to the view, and this variable is an integer.
-
+#looks like it is best to include a / at the end of the url path.
